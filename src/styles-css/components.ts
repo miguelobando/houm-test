57fde82a69ea1097c0eda28bffdfb0354c71e842
Miguel Obando <miguel.obando204@gmail.com>
2022-05-11 03:01:36 -0400

    control point

--- src/styles-css/components.ts
+++ src/styles-css/components.ts
@@ -1,7 +1,6 @@
 import { Card, Chip, styled, Typography } from "@mui/material";
 
 export const CardHoum = styled(Card)({
-  position: "relative",
   borderRadius: "12px",
   boxShadow:
     "0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)",
